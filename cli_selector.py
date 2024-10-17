92c30a79a914b39f264dab956c7df50a6910acbf
s1YH0pper <1147083390@qq.com>
2024-10-18 00:33:30 +0800

    :tada: :sparkles: Update CLI menu, add menu options

---
+++ cli_selector.py
@@ -0,0 +1,57 @@
+from InquirerPy import prompt
+import subprocess
+
+
+# 运行目录下 Python 文件的函数
+def run_python_file(file_path):
+    try:
+        # 使用 subprocess 调用子目录中的 Python 文件
+        subprocess.run(["python", file_path], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while running {file_path}: {e}")
+
+
+# 子菜单：运行子目录下的 Python 文件
+def submenu(menu_choice: str, submenu_dict: dict):
+    while True:
+        # 动态生成choices列表，末尾添加"Back"选项
+        choices = list(submenu_dict.keys()) + ["Back"]
+        questions = [
+            {
+                "type": "list",
+                "message": f"{menu_choice} - 选择要运行的:",
+                "choices": choices,
+                "name": "submenu_choice",
+            }
+        ]
+        answer = prompt(questions)
+
+        choice = answer["submenu_choice"]
+        if choice == "Back":
+            break  # 返回上一级菜单
+        else:
+            run_python_file(submenu_dict[choice])  # 根据选项运行对应的Python文件
+
+
+# 主菜单
+def main_menu(menu_dict: dict):
+    while True:
+        # 动态生成choices列表，末尾添加"Exit"选项
+        choices = list(menu_dict.keys()) + ["Exit"]
+        questions = [
+            {
+                "type": "list",
+                "message": "Main Menu - Select an option:",
+                "choices": choices,
+                "name": "menu_choice",
+            }
+        ]
+        answer = prompt(questions)
+
+        if answer["menu_choice"] != "Exit":
+            submenu(
+                answer["menu_choice"], menu_dict[answer["menu_choice"]]
+            )  # 进入子菜单
+        elif answer["menu_choice"] == "Exit":
+            print("Exiting...")
+            break  # 退出程序
---
+++ main.py
@@ -0,0 +1,28 @@
+import cli_selector
+
+
+basic_menu_options = {
+    "1. 基本图像处理": "basic-1/main.py",
+    "2. 噪声及滤波处理": "basic-2/main.py",
+    "3. 图像的形态学运算": "basic-3/main.py",
+    "4. 图像的平滑和锐化": "basic-4/main.py",
+    "5. 医学图像 DCT 水印": "basic-5/main.py",
+    "6. CT 图像肺实质分割 (未完成)": "basic-6/main.py",
+    "7. 医学图像数值的测量 (未完成)": "basic-7/main.py",
+    "8. 医学图像伪彩处理 (未完成)": "basic-8/main.py",
+}
+
+advanced_menu_options = {
+    "1. 退化医学图像的复原 (未完成)": "advanced-1/main.py",
+    "2. 基于 SIFT 算法的医学图像配准": "advanced-2/main.py",
+    "3. 不同模态医学图像的融合 (未完成)": "advanced-3/main.py",
+    "4. 医学图像病变部位的标记 (未完成)": "advanced-4/main.py",
+    "5. CT 图像的窗宽和窗位 (未完成)": "advanced-5/main.py",
+    "6. MRI 图像的频率域滤波": "advanced-6/main.py",
+    "7. MRI 图像的增强处理 (未完成)": "advanced-7/main.py",
+    "8. 基于分水岭算法的病变区域分割 (未完成)": "advanced-8/main.py",
+}
+menu_dict = {"基础(basic)": basic_menu_options, "高级(advanced)": advanced_menu_options}
+
+if __name__ == "__main__":
+    cli_selector.main_menu(menu_dict)
